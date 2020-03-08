7db098c307ededa63c1a54c0ac1f2036ad35f7ac
Christian Donnerer <christian.donnerer@gmail.com>
2020-03-08 20:59:24 +0000

    this is spectacularly bad code

--- src/pyats/symmetry.py
+++ src/pyats/symmetry.py
@@ -1,21 +1,27 @@
+"""Space group symmetries
+"""
 import os
 import numpy as np
+
+
+TOL = 1e-5
+
 
 class Symmetry(object):
     def __init__(self, sym):
         self.sym = sym
         self.symOp, self.symTr = self.generate_operators(sym)
 
-    def generate_operators(self,sym):
-        """
-        Generate spacegroup symmetry operations
+    def generate_operators(self):
+        """Generate spacegroup symmetry operations
 
-        :param sym:
-        :return: symOp, symTr:
-        """
+        Returns
+        -------
+        type
+            Description of returned object.
 
-        tol = 1e-5
-        gener, trans, symStr = self.generate_symmetry(sym)
+        """
+        gener, trans, symStr = self.generate_symmetry(self.sym)
 
         nGen = len(gener[1, 1, :])
         symOp = []
@@ -40,23 +46,25 @@ class Symmetry(object):
                     RS = np.dot(R, symOp[k])
                     TS = (R.dot(symTr[k]) + T) % 1
 
-                    idxR = np.sum(np.sum(abs(symOp - RS), axis=1), axis=1) > tol
-                    idxT = np.sum(abs(symTr - TS), axis=1) > tol
+                    idxR = np.sum(np.sum(abs(symOp - RS), axis=1), axis=1) > TOL
+                    idxT = np.sum(abs(symTr - TS), axis=1) > TOL
 
                     if all(idxT | idxR):
                         symTr.append(TS)
                         symOp.append(RS)
         return symOp, symTr
 
-    def generate_symmetry(self,sym):
+    def generate_symmetry(self, sym):
         """
         Read in spacegoup generators from file
 
         :param sym:
         :return: symOp, symTr, symStr:
         """
-        fn = os.path.join(os.path.dirname(__file__),'dat_files'+os.sep+'symmetry.dat')
-        f = open(fn,'r')
+        fn = os.path.join(
+            os.path.dirname(__file__), "dat_files" + os.sep + "symmetry.dat"
+        )
+        f = open(fn, "r")
 
         for line in f:
             if sym in line:
@@ -72,39 +80,39 @@ class Symmetry(object):
         nSign = 1
 
         j = 0
-        while (j < len(symStr)):
-            if symStr[j] == ',':
+        while j < len(symStr):
+            if symStr[j] == ",":
                 symOp[nNew, :, nOp] = vNew
                 vNew *= 0
                 nSign = 1
                 nNew = nNew % 2 + 1
-            elif symStr[j] == ';':
+            elif symStr[j] == ";":
                 symOp[nNew, :, nOp] = vNew
                 vNew *= 0
                 nSign = 1
                 nNew = 0
                 nOp += 1
-            elif symStr[j] == 'x':
+            elif symStr[j] == "x":
                 vNew[0] = nSign
-            elif symStr[j] == 'y':
+            elif symStr[j] == "y":
                 vNew[1] = nSign
-            elif symStr[j] == 'z':
+            elif symStr[j] == "z":
                 vNew[2] = nSign
-            elif symStr[j] == '-':
+            elif symStr[j] == "-":
                 nSign = -1
-            elif symStr[j] == '+':
+            elif symStr[j] == "+":
                 nSign = 1
-            elif symStr[j] == '1' or symStr[j] == '2' or symStr[j] == '3':
+            elif symStr[j] == "1" or symStr[j] == "2" or symStr[j] == "3":
                 symTr[nNew, nOp] = float(symStr[j]) / float(symStr[j + 2])
                 j += 2
             j += 1
 
         symOp[nNew, :, nOp] = vNew
-        symOp = symOp[:, :, 0:nOp+1]
-        symTr = symTr[:, 0:nOp+1]
+        symOp = symOp[:, :, 0 : nOp + 1]
+        symTr = symTr[:, 0 : nOp + 1]
         return symOp, symTr, symStr
 
-    def symmetry_order(self,R, T):
+    def symmetry_order(self, R, T):
         """
         Determine order of symmetry operator
 
@@ -114,11 +122,13 @@ class Symmetry(object):
         """
         tol = 1e-5
 
-        N  = 1
+        N = 1
         RN = R
         TN = T
 
-        while (np.linalg.norm(RN - np.identity(3)) > tol or np.linalg.norm(TN) > tol) and N < 10:
+        while (
+            np.linalg.norm(RN - np.identity(3)) > tol or np.linalg.norm(TN) > tol
+        ) and N < 10:
             RN = np.dot(R, RN)
             TN = (R.dot(TN) + T) % 1
             N += 1
@@ -164,23 +174,25 @@ class Symmetry(object):
 
         anisoT = np.ones(shape=(3, 3)) - np.identity(3)
 
-        b  = np.exp(2j * np.pi * np.einsum('i,ji->j', q, (self.symTr + np.dot(self.symOp, r))))
-        a  = np.einsum('mij,jk,mlk->mil', self.symOp, anisoT, self.symOp)
-        Fk = np.einsum('ijk,i->jk', a, b)
+        b = np.exp(
+            2j * np.pi * np.einsum("i,ji->j", q, (self.symTr + np.dot(self.symOp, r)))
+        )
+        a = np.einsum("mij,jk,mlk->mil", self.symOp, anisoT, self.symOp)
+        Fk = np.einsum("ijk,i->jk", a, b)
 
         self.chop(Fk)
         norm = Fk.sum() / 2
 
         # Check if tensor is non-zero
         with np.warnings.catch_warnings():
-            np.warnings.filterwarnings('error')
+            np.warnings.filterwarnings("error")
             try:
                 return Fk / norm
             except:
-                print('Reflection Q = (', q[0], q[1], q[2],') is not ATS allowed!')
+                print("Reflection Q = (", q[0], q[1], q[2], ") is not ATS allowed!")
                 return None
 
-    def chop(self,a):
+    def chop(self, a):
         """
         Helper function to get rid of numerical errors
 
